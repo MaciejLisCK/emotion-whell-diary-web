6448ce2a81b8bcc7d8e35922034efec278ac7fb8
M̦͈͈̳̭͚͇̳̙̙͍̱͖̺͈ͥ̽̿̈́̇ͭ̾̍ͮ̒̓́ͯ͛͆ͅaciej Lis <accounts@maciejlis.com>
2024-12-21 10:23:05 +0000

    format date display in emotion list

--- src/app/emotion-list/emotion-list.component.ts
+++ src/app/emotion-list/emotion-list.component.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmotionsState, EmotionsStateModel } from '../emotions/emotions.state';
 import { Store } from '@ngxs/store';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-emotion-list',
-  imports: [ AsyncPipe ],
+  imports: [ AsyncPipe, DatePipe ],
   templateUrl: './emotion-list.component.html',
   styleUrl: './emotion-list.component.scss'
 })
