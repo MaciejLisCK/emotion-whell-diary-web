b1a20c94632d4e0053da10101dbbff42afc91604
M̦͈͈̳̭͚͇̳̙̙͍̱͖̺͈ͥ̽̿̈́̇ͭ̾̍ͮ̒̓́ͯ͛͆ͅaciej Lis <accounts@maciejlis.com>
2024-12-21 01:19:52 +0000

    add emotion output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { EmotionWhellComponent } from './emotion-whell/emotion-whell.component';
+import { EmotionListComponent } from './emotion-list/emotion-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ EmotionWhellComponent],
+  imports: [ EmotionWhellComponent, EmotionListComponent ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  host: { 'class': 'container' },
 })
 export class AppComponent {
   title = 'emotion-whell-diary-web';
---
+++ src/app/emotion-list/emotion-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emotion-list',
+  imports: [],
+  templateUrl: './emotion-list.component.html',
+  styleUrl: './emotion-list.component.scss'
+})
+export class EmotionListComponent {
+
+}
